2c5f199e1826c61632eeb5ff93a959ef98c32ddc
Tester_001 <glaywang.2046@gmail.com>
2026-08-08 23:33:32 +0800

    Revert "简化版本：测试基本运行"
    This reverts commit 511113a4a83ebff51df02b71dca9b7a1b8ac9f07.

--- ProxyManager/MainWindow.xaml.cs
+++ ProxyManager/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
+using System.ComponentModel;
 using System.Windows;
+using ProxyManager.Services;
+using ProxyManager.ViewModels;
 
 namespace ProxyManager;
 
@@ -7,5 +10,29 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        
+        var settingsService = new SettingsService();
+        var scanner = new Scanner();
+        var launcher = new Launcher();
+        var themeManager = new ThemeManager(settingsService);
+        
+        DataContext = new MainViewModel(scanner, launcher, themeManager, settingsService);
+        
+        Closing += OnClosing;
+        
+        // 启动时自动扫描
+        if (DataContext is MainViewModel vm)
+        {
+            vm.ScanCommand.Execute(null);
+        }
+    }
+
+    private void OnClosing(object? sender, CancelEventArgs e)
+    {
+        if (App.Current is App app && app.MinimizeToTray)
+        {
+            e.Cancel = true;
+            Hide();
+        }
     }
 }
